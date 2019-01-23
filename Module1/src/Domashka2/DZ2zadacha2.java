45ad9d9ebb7a54f6b903ec138f1698cf049c940a
Apelsini <Apelsin652@gmail.com>
2019-01-24 01:04:37 +0300

    готовая задача домашки 2

--- Module1/src/Domashka2/DZ2zadacha2.java
+++ Module1/src/Domashka2/DZ2zadacha2.java
@@ -1,4 +1,31 @@
 package Domashka2;
-
+import java.util.Scanner;
 public class DZ2zadacha2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("введите число, меньше которого надо вывести числа Фибоначчи:");
+        int a = sc.nextInt();
+        fibon(a);
+    }
+    /*
+    Написать программу, вычисляющую и отображающую все числа
+    Фибоначчи меньше введённого пользователем целого числа.
+Решить двумя способами:
+с помощью цикла
+с помощью рекурсии.
+     */
+    public static void fibon (int b){
+        int fib1=1;
+        int fib2 =1;
+        int fib3=1;
+        System.out.println(fib1);
+        while (fib3<b) {
+            System.out.println(fib3);
+            fib3= fib1+fib2;
+            fib1=fib2;
+            fib2=fib3;
+        }
+    }
+
 }
