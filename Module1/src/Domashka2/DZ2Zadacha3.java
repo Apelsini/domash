2643ad3a769ba24138187260d350366c3ceb560f
Apelsini <Apelsin652@gmail.com>
2019-01-27 22:31:07 +0300

    готовые задачи домашки

--- Module1/src/Domashka2/DZ2Zadacha3.java
+++ Module1/src/Domashka2/DZ2Zadacha3.java
@@ -3,26 +3,27 @@ package Domashka2;
 import java.util.Scanner;
 
 public class DZ2Zadacha3 {
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
 
-  //  System.out.println("введите число дней")
-    int n = sc.nextInt();
-    int bsum = 0;
-    int dmax = 0;
-    for(int i = 1;i<n; i++)
+        Scanner sc = new Scanner(System.in);
 
-    {
-        System.out.println("введите количество осадков за день " + i);
-        int curr = sc.nextInt();
-        System.out.println("всего за " + i + " дней:");
-        bsum = bsum + curr;
-        System.out.println("сумма осадков =" + bsum);
-        System.out.println("среднее количество осадков =" + bsum / i);
-        if (curr > dmax) {
-            dmax = curr;
+        System.out.println("введите число дней")
+        int n = sc.nextInt();
+        int bsum = 0;
+        int dmax = 0;
+        for (int i = 1; i < n; i++) {
+            System.out.println("введите количество осадков за день " + i);
+            int curr = sc.nextInt();
+            System.out.println("всего за " + i + " дней:");
+            bsum = bsum + curr;
+            System.out.println("сумма осадков =" + bsum);
+            System.out.println("среднее количество осадков =" + bsum / i);
+            if (curr > dmax) {
+                dmax = curr;
+            }
+            System.out.println("максимальное количество осадков =" + dmax);
+
+
         }
-        System.out.println("максимальное количество осадков =" + dmax);
-
-
     }
 }
