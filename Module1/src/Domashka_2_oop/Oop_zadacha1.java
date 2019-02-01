da4bb74c8f5e9e5d79a3f20a495f7b3331df23c1
Apelsin <tre21jfk$>
2019-02-01 17:47:43 +0300

    загружена 1 задача дз 3 финальная, с проверкой

--- Module1/src/Domashka_2_oop/Oop_zadacha1.java
+++ Module1/src/Domashka_2_oop/Oop_zadacha1.java
@@ -2,10 +2,14 @@ package Domashka_2_oop;
 
 public class Oop_zadacha1 {
     public static void main(String[] args) {
-        Point a = new Point(5,3);
-        System.out.println("расстояние "+a.distance(a,10,6));
+        Point a = new Point(5, 3);
+        System.out.println("расстояние " + a.distance(a, 10, 6));
+
+        Point p = new Point(3, 4).translate(1, 3).scale(2);
+        System.out.println("точка имеет координаты X=" + p.getX() + " и Y=" + p.getY());
+        System.out.println("проверка");
+        Point t = new Point(3, 4).translate(1, 3);
+        System.out.println("проверочная точка имеет координаты X=" + p.getX() + " и Y=" + p.getY());
 
     }
-
-
 }
--- Module1/src/Domashka_2_oop/Point.java
+++ Module1/src/Domashka_2_oop/Point.java
@@ -1,26 +1,55 @@
 package Domashka_2_oop;
 
 public class Point {
-    /** Создать класс Point, описывающий точку в двумерных координатах.
-     Полями этого класса должны быть координаты x и y.
-     Класс должен иметь один конструктор, принимающий координаты x и y.
-     Сделайте информацию о координатах скрытой, а получить её можно
-     только с помощью методов доступа (get/set).
-
-     // методы доступа пока не проходили.
-
-     Создать метод distance,
-     принимающий объект Point и определяющий расстояние до неё.
+    /**
+     * Создать класс Point, описывающий точку в двумерных координатах.
+     * Полями этого класса должны быть координаты x и y.
+     * Класс должен иметь один конструктор, принимающий координаты x и y.
+     * Сделайте информацию о координатах скрытой, а получить её можно
+     * только с помощью методов доступа (get/set).
+     * <p>
+     * // методы доступа пока не проходили.
+     * <p>
+     * Создать метод distance,
+     * принимающий объект Point и определяющий расстояние до неё.
      */
-   static int x;
+    static int x;
     static int y;
+
+    public void setX(int x) {
+        Point.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        Point.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public double distance (Point a, int xxx, int yyy) {
-        return Math.sqrt((xxx-a.x)*(xxx-a.x)+(yyy-a.y)*(yyy-a.y));
+    public double distance(Point a, int xxx, int yyy) {
+        return Math.sqrt((xxx - a.x) * (xxx - a.x) + (yyy - a.y) * (yyy - a.y));
+    }
+
+    public double distancePoint(Point a, Point b) {
+        return this.distance(a, b.getX(), b.getY());
+    }
+
+    public Point translate(int shiftX, int shiftY) {
+        return new Point(this.getX()+shiftX, this.getY()+shiftY);
+    }
+
+    public Point scale (int zoom) {
+        return new Point (Math.round(this.getX()/zoom), Math.round(this.getY()/zoom));
     }
 }
