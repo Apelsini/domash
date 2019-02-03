61600f3b3862d886e7ee9879e8237b7e90ccf31f
Apelsini <Apelsin652@gmail.com>
2019-02-03 22:54:29 +0300

    задача про Car начало

--- Module1/src/Domashka_2_oop/Car.java
+++ Module1/src/Domashka_2_oop/Car.java
@@ -4,6 +4,33 @@ public class Car {
 
     static int position;
     static float rasx;
+    static float benzobak;
+    static float benzomax;
+    static String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        Car.name = name;
+    }
+
+    public void setBenzomax(float benzomax) {
+        Car.benzomax = benzomax;
+    }
+
+    public float getBenzomax() {
+        return benzomax;
+    }
+
+    public float getBenzobak() {
+        return benzobak;
+    }
+
+    public void setBenzobak(float benzobak) {
+        Car.benzobak = benzobak;
+    }
 
     public float getRasx() {
         return rasx;
@@ -13,16 +40,50 @@ public class Car {
         Car.rasx = rasx;
     }
 
-    public void setPosition(int position) {
-        this.position = position;
-    }
-
     public int getPosition() {
         return position;
     }
 
-    public Car(int initialPosition, float rasxod) {
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+
+    public Car(String name, int initialPosition, float rasxod, float benzomax) {
+        this.setName(name);
         this.setPosition(initialPosition);
         this.setRasx(rasxod);
+        this.setBenzomax(benzomax);
+    }
+
+    public void fillbenzobak(float fillgas) {
+        if (fillgas > 0) {
+            if ((this.getBenzobak() + fillgas) > this.getBenzomax()) {
+                this.setBenzobak(this.getBenzomax());
+            } else {
+                this.setBenzobak(this.getBenzobak() + fillgas);
+            }
+        } else {
+            if ((this.getBenzobak() + fillgas) <0) {
+                this.setBenzobak(0);
+            } else {
+                this.setBenzobak(this.getBenzobak() + fillgas);
+            }
+        }
+
+
+    }
+
+    public void move(int km) {
+        float spare = km*this.getRasx()/100;
+        if (spare>this.getBenzobak()) {
+            km = (int) Math.floor(this.getBenzobak()/this.getRasx()*100);
+        }
+        this.setPosition(km);
+        this.fillbenzobak(-1*spare);
+    }
+
+    public int distanceleft() {
+        return (int) Math.floor(this.getBenzobak()/this.getRasx()*100);
     }
 }
--- Module1/src/Domashka_2_oop/Oop_zadacha2Car.java
+++ Module1/src/Domashka_2_oop/Oop_zadacha2Car.java
@@ -2,6 +2,14 @@ package Domashka_2_oop;
 
 public class Oop_zadacha2Car {
     public static void main(String[] args) {
-
+        Car porsche = new Car("Porsche", 0, 15.7f, 65.0f);
+        System.out.println("заведена машина " + porsche.getName() + " с расходом " + porsche.getRasx() + " которая стоит на позиции " + porsche.getPosition() + " и в баке у нее " + porsche.getBenzobak() + " л." + " в баке помещается до " + porsche.getBenzomax());
+        porsche.fillbenzobak(65.5f);
+        System.out.println("заправлена машина " + porsche.getName() + " с расходом " + porsche.getRasx() + " которая стоит на позиции " + porsche.getPosition() + " и в баке у нее " + porsche.getBenzobak() + " л." + " в баке помещается до " + porsche.getBenzomax());
+        int move = 320;
+        porsche.move(move);
+        System.out.println("передвигаем машину на "+move+" км. Это потребует "+porsche.getRasx()*move/100+" л.");
+        System.out.println("перемещена машина " + porsche.getName() + " с расходом " + porsche.getRasx() + " которая стоит на позиции " + porsche.getPosition() + " и в баке у нее " + porsche.getBenzobak() + " л." + " в баке помещается до " + porsche.getBenzomax());
+        System.out.println(porsche.getName()+" еще может проехать "+porsche.distanceleft()+" км.");
     }
 }
